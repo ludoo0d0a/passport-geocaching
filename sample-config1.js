78bfad80550feaaa23041f3fd61cca4f1b5975d2
ludoo0d0a <ludovic.valente@gmail.com>
2015-08-11 10:08:23 +0000

    Add sample config

---
+++ sample-config1.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: '1A1A1A1A-1A1A-1A1A-1A1A-1A1A1A1A1A1A1A',
+    consumer_secret: '5A5A5A5A-5A5A-5A5A-5A5A-5A5A5A5A',
+    callbackURL: 'https://localhost/auth/geocaching/callback',
+    
+    live: false
+};
