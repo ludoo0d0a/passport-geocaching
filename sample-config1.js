239d05506a48c4fbee0847ec9b45f0fc3d015960
Surfoo <surfooo@gmail.com>
2015-10-12 08:58:16 +0200

    Fixed options

--- sample-config1.js
+++ sample-config1.js
@@ -1,6 +1,6 @@
 module.exports = {
     consumerKey: '1A1A1A1A-1A1A-1A1A-1A1A-1A1A1A1A1A1A1A',
     consumerSecret: '5A5A5A5A-5A5A-5A5A-5A5A-5A5A5A5A',
-    callbackURL: 'https://localhost/auth/geocaching/callback',
-    live: false
+    callbackURL: 'https://localhost:3000/auth/geocaching/callback',
+    env: 'staging'
 };
