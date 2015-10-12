56c0e57a3ea77c9051e74304e6b0c358d6cee714
Surfoo <surfooo@gmail.com>
2015-10-12 08:48:18 +0200

    Fixed keys for the demo

--- sample-config1.js
+++ sample-config1.js
@@ -1,7 +1,6 @@
 module.exports = {
-    consumer_key: '1A1A1A1A-1A1A-1A1A-1A1A-1A1A1A1A1A1A1A',
-    consumer_secret: '5A5A5A5A-5A5A-5A5A-5A5A-5A5A5A5A',
+    consumerKey: '1A1A1A1A-1A1A-1A1A-1A1A-1A1A1A1A1A1A1A',
+    consumerSecret: '5A5A5A5A-5A5A-5A5A-5A5A-5A5A5A5A',
     callbackURL: 'https://localhost/auth/geocaching/callback',
-    
     live: false
 };
