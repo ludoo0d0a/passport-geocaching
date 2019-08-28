b7f1c6c76927145dc8f52b345691c390b8a87522
LudoO <ludovic.valente@gmail.com>
2019-08-28 18:50:56 +0200

    Remove sample in a separate repo

--- default-config-api.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    clientID: '123456789-ABCD-123456789-ABCD',
-    clientSecret: '123456789-ABCD-123456789-ABCD',
-    callbackURL: 'https://localhost:3000/auth/callback',
-    env: 'staging'
-};
